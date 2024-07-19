147d59208af48d7cd53eca5dada6d64836abae21
stipulus <tyler@stipulus.com>
2024-07-19 13:53:30 -0600

    built resources

--- dist/codebaby-react.esm.js
+++ dist/codebaby-react.esm.js
@@ -1,4 +1,4 @@
-import p, { useState as g, useLayoutEffect as C, createContext as E, forwardRef as R, useCallback as u, useMemo as k, useImperativeHandle as x, useEffect as h, Fragment as A, useContext as S } from "react";
+import p, { useState as g, useLayoutEffect as E, createContext as R, forwardRef as k, useCallback as u, useMemo as x, useImperativeHandle as z, useEffect as h, Fragment as A, useContext as S } from "react";
 const q = {
   tansformEventName(e) {
     return this.types[e] ? this.types[e] : e;
@@ -38,14 +38,14 @@ const q = {
     camChange: "na-camChange.vidbaby",
     glbLoaded: "na-glbLoaded.vidbaby"
   }
-}, l = q;
-function w(e) {
-  const a = e.replace(/([A-Z])/g, (n) => `-${n[0].toLowerCase()}`);
-  return Object.keys(l.types).includes(a) ? a : e;
+}, v = q;
+function f(e) {
+  const a = e.replace(/([A-Z])/g, (t) => `-${t[0].toLowerCase()}`);
+  return Object.keys(v.types).includes(a) ? a : e;
 }
-const j = ({ src: e, type: a = "module", async: n = !0 }) => new Promise((c, r) => {
+const j = ({ src: e, type: a = "module", async: t = !0 }) => new Promise((c, r) => {
   const i = document.createElement("script");
-  i.src = e, i.type = a, i.async = n, i.onload = () => c(i), i.onerror = () => r(new Error("Failed to load script. Check your internet connection.")), document.head.appendChild(i);
+  i.src = e, i.type = a, i.async = t, i.onload = () => c(i), i.onerror = () => r(new Error("Failed to load script. Check your internet connection.")), document.head.appendChild(i);
 });
 async function L() {
   if (typeof window.jQuery > "u" || typeof window.$ != "function" || window.jQuery.fn.jquery === void 0 || window.jQuery.fn.jquery < "3.4.1") {
@@ -59,81 +59,84 @@ async function L() {
     window.vidbaby$ = window.$;
   typeof window.$ > "u" && (window.$ = window.jQuery);
 }
-function z({ id: e, env: a }) {
-  const [n, c] = g();
-  return C(() => {
+function N({ id: e, env: a }) {
+  const [t, c] = g();
+  return E(() => {
     const r = async () => {
       const i = {
         local: `http://localhost:8080/loader.js?id=${e}&env=local`,
         dev: `https://bridge3-dev.codebaby.com/loader.js?id=${e}&env=dev`,
         qa: `https://qa-avatar.n-avatars.com/loader.js?id=${e}&env=qa`,
         default: `https://portal.codebaby.com/loader.js?id=${e}`
-      }, o = (typeof a == "string" ? a : "").toLowerCase().replace("localhost", "local"), y = i[o] || i.default;
-      await L(), await j({ src: y }), c(() => window.vidbaby$);
+      }, o = (typeof a == "string" ? a : "").toLowerCase().replace("localhost", "local"), l = i[o] || i.default;
+      await L(), await j({ src: l }), c(() => window.vidbaby$);
     };
     window ? r() : setTimeout(r, 3e3);
-  }, []), n;
+  }, []), t;
 }
-const N = E({
+const P = R({
   on(e, a) {
   },
   off(e, a) {
   },
   trigger(e, a) {
   }
-}), $ = N, I = R(function({ children: a, id: n, env: c, ...r }, i) {
-  const o = z({ id: n, env: c }), y = u((d, s) => {
-    o && o(document).on(l.tansformEventName(d), s);
-  }, [o]), f = u((d, s) => {
-    o && o(document).off(l.tansformEventName(d), s);
+}), $ = P, Q = k(function({ children: a, id: t, env: c, ...r }, i) {
+  const o = N({ id: t, env: c }), l = u((d, s) => {
+    o && o(document).on(v.tansformEventName(d), s);
   }, [o]), m = u((d, s) => {
-    o && o(document).trigger(l.tansformEventName(d), s);
-  }, [o]), b = k(() => ({
-    on: y,
-    off: f,
-    trigger: m
-  }), [y, f, m]);
-  return x(i, function() {
+    o && o(document).off(v.tansformEventName(d), s);
+  }, [o]), w = u((d, s) => {
+    o && o(document).trigger(v.tansformEventName(d), s);
+  }, [o]), b = x(() => ({
+    on: l,
+    off: m,
+    trigger: w
+  }), [l, m, w]);
+  return z(i, function() {
     return b;
   }, [b]), h(() => {
     if (!o || !r || Object.keys(r).length === 0)
       return;
-    const d = r, s = Object.keys(r).filter((t) => t.startsWith("on")).map((t) => t.replace("on", "")).map((t) => t.charAt(0).toLowerCase() + t.slice(1));
+    const d = r, s = Object.keys(r).filter((n) => n.startsWith("on")).map((n) => n.replace("on", "")).map((n) => n.charAt(0).toLowerCase() + n.slice(1));
     if (!(s.length <= 0)) {
-      for (const t of s) {
-        const v = `on${t.charAt(0).toUpperCase() + t.slice(1)}`;
-        b.on(w(t), d[v]);
+      for (const n of s) {
+        console.log(n);
+        const y = `on${n.charAt(0).toUpperCase() + n.slice(1)}`;
+        n == "initialized" ? b.on(f(n), (...C) => {
+          console.log("initialized event"), setTimeout(() => d[y].apply(null, ...C), 1700);
+        }) : b.on(f(n), d[y]);
       }
       return () => {
-        for (const t of s) {
-          const v = `on${t.charAt(0).toUpperCase() + t.slice(1)}`;
-          b.off(w(t), d[v]);
+        for (const n of s) {
+          const y = `on${n.charAt(0).toUpperCase() + n.slice(1)}`;
+          b.off(f(n), d[y]);
         }
       };
     }
   }, [r, o]), /* @__PURE__ */ p.createElement($.Provider, { value: b }, /* @__PURE__ */ p.createElement(A, null, a, /* @__PURE__ */ p.createElement("div", { id: "avatarContainer", className: "codebaby-react-avatar-container" })));
 });
-function Q() {
-  const { on: e, off: a, trigger: n } = S($);
-  return { on: e, off: a, trigger: n };
-}
 function D() {
+  const { on: e, off: a, trigger: t } = S($);
+  return { on: e, off: a, trigger: t };
+}
+function B() {
   const [e, a] = g(!1);
   return h(() => {
-    let n = null;
+    let t = null;
     const c = () => {
       if (window && window.vidbaby$ && window.vidbaby && window.vidbaby.initialized)
         return a(!0);
-      n = setTimeout(c, 1e3);
+      t = setTimeout(c, 1e3);
     };
     return c(), () => {
-      n && clearTimeout(n);
+      t && clearTimeout(t);
     };
   }, []), e;
 }
 export {
-  I as AvatarProvider,
-  Q as useCodebabyEvents,
-  D as useIsInitialized
+  Q as AvatarProvider,
+  D as useCodebabyEvents,
+  B as useIsInitialized
 };
 //# sourceMappingURL=codebaby-react.esm.js.map
